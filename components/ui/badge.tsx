36ee0dc7546451b245079b135f459afe4e158159
Alessia Frisoni <74038510+frisonialessia@users.noreply.github.com>
2026-06-20 23:36:33 +0200

    Create badge.tsx

---
+++ components/ui/badge.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+interface BadgeProps extends React.HTMLAttributes<HTMLSpanElement> {
+  color?: string;
+}
+
+export function Badge({ className, color, style, ...props }: BadgeProps) {
+  return (
+    <span
+      className={cn(
+        "inline-block rounded-md px-2.5 py-1 text-[11.5px] font-semibold",
+        className
+      )}
+      style={{
+        ...(color ? { background: `${color}22`, color } : {}),
+        ...style,
+      }}
+      {...props}
+    />
+  );
+}
